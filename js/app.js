d3de03d1e4946dd27350dce4c91d74e9ccbf2805
samantha <langridge.se@gmail.com>
2022-04-08 21:07:56 -0600

    attempted second function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//function for getting a random activity upon clicking
 function randomActivityGenerator(){
     axios.request({
         method:"GET",
@@ -7,12 +8,38 @@ function randomActivityGenerator(){
 
 function activityFound (response){
     console.log(response);
-    document.getElementById("randomActivity").innerText=response.data.activity;
+    document.getElementById("randomActivity").innerText=response.data.activity; //choosing a random activity in the array
 }
 
 function activityFail (error){
     console.log(error);
     document.getElementById("randomActivity").innerText="failed to get activity";
 }
-
+//creating an event listener for clicking the "get random activity" button
 document.getElementById("randomActivityButton").addEventListener('click', randomActivityGenerator);
+
+
+//function for activity based on number of participants
+
+function recommendedActivity(){
+    axios.request({
+        method : "GET",
+        url:"https://www.boredapi.com/api/activity?participants=:participants",
+        data:{
+            participants:document.getElementById("participantNumber").value
+        }
+    }).then(recommendedSuccess).catch(recommendedFail);
+}
+
+function recommendedSuccess(response){
+    console.log(response);
+    document.getElementById("recommendedActivity").innerText = response.data.value;
+}
+
+function recommendedFail (error){
+    console.log(error);
+}
+
+document.getElementById("recommendedActivityButton").addEventListener('click', recommendedActivity);
+
+//I couldn't figure out how to link the option to the response
